728cf37e29100b22bd93d5068fceae1cf46725fa
academiainteligente <academiainteligente.ai@gmail.com>
2026-04-14 02:20:00 -0600

    Agrega ruta de Directorio de Apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Eventos from './pages/Eventos';
+import DirectorioApps from './pages/DirectorioApps';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/eventos" element={<Eventos />} />
+        <Route path="/directorio-apps" element={<DirectorioApps />} />
       </Routes>
     </Router>
   );
